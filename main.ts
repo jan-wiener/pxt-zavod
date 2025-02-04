de5127b2d3ca3c25373d48a93258ec78256ed859
Jan Wiener <jan.wiener.024@pslib.cz>
2025-02-04 09:36:54 +0100

    timecalib

--- main.ts
+++ main.ts
@@ -18,6 +18,7 @@ console.log(whoAmI)
 let mode: STATE = STATE.ready;
 mode = STATE.finish
 
+let timeCalib: number
 
 Sensors.SetLightLevel()
 basic.showNumber(0)
@@ -26,42 +27,57 @@ let startTime: number
 let totalTime: number
 
 
-//radio.onReceivedNumber(function(receivedNumber: number) {
-//    mode = STATE.running
-//    startTime = receivedNumber
-//})
 
 radio.onReceivedValue(function(name: string, value: number) {
-    if (name == "start") {
-        mode = STATE.running
-        startTime = control.millis()
-    } else if (name == "time") {
+    if (name == "calib") { //to end; recieve timecalib val
+        timeCalib = value
+    } else if (name == "time") { // to start; total time sent from end to start
         mode = STATE.finish
         totalTime = value
         console.log(`Závod trval ${totalTime}`)
     }
 })
 radio.onReceivedString(function(receivedString: string) {
-    if (receivedString == "startSet") {
+    if (receivedString == "startSet") { //to end; set state to end
         whoAmI = WHOAMI.end
         basic.showNumber(2)
-    } else if (receivedString == "stop") {
+        music.playTone(100, 200)
+        radio.sendString("calib") // return calib 
+
+    } else if (receivedString == "stop") { // universal; stop
         mode = STATE.finish
+
+    } else if (receivedString == "start") { // to end; start counting time
+        mode = STATE.running
+        startTime = control.millis()
+
+    } else if (receivedString == "calib") { // to start; calculate calib time
+        timeCalib = (control.millis() - timeCalib) / 2 //time to send a string in ms
+        console.log(timeCalib)
+        music.playTone(500, 300)
+        radio.sendValue("calib", timeCalib) // send calib time in ms to end
     }
 })
 
+// get time to send a string
+// the race begins
+// because sending a str takes time, timer at WHOAMI.end begins later
+// because of this, the program will add the time to send a str at the end (timeCalib)
+
+// WARNING: if the race takes less time than to send a str, it will not work
 
 Sensors.OnLightDrop(function () {
 
-    if (mode == STATE.ready && whoAmI == WHOAMI.start) {
+    if (mode == STATE.ready && whoAmI == WHOAMI.start) { //begin race on WHOAMI.start if ready 
         music.playTone(100, 200)
         mode = STATE.running
-        radio.sendValue("start", 1)
+        radio.sendString("start") // send start info
 
-    } else if (mode == STATE.running && whoAmI == WHOAMI.end) {
+    } else if (mode == STATE.running && whoAmI == WHOAMI.end) { // end race on WHOAMI.end if running
         music.playTone(100, 200)
         mode = STATE.finish
-        totalTime = control.millis() - startTime
+        totalTime = control.millis() - startTime // calculate total time
+        totalTime += timeCalib // add send time
         console.log(`Závod trval ${totalTime}`) //basic.shownumber když to nefunguje
         radio.sendValue("time", totalTime)
 
@@ -70,6 +86,7 @@ Sensors.OnLightDrop(function () {
 
     }
 })
+
 
 input.onButtonPressed(Button.A, function() {
     if (whoAmI == WHOAMI.start && mode == STATE.finish) {
@@ -80,16 +97,20 @@ input.onButtonPressed(Button.A, function() {
     } else if (whoAmI != WHOAMI.end && mode == STATE.finish) {
         whoAmI = WHOAMI.start
         basic.showNumber(1)
+        timeCalib = control.millis() // 
+        music.playTone(100, 200)
         radio.sendString("startSet")
         basic.pause(2000)
-        mode = STATE.ready
-        music.playTone(200, 200)
+        //mode = STATE.ready
+        //music.playTone(100, 200)
 
     } else if (whoAmI == WHOAMI.start && mode == STATE.ready) {
         mode = STATE.finish
+        music.playTone(100, 200)
 
-    } else if (whoAmI == WHOAMI.start && mode == STATE.running) {
+    } else if (mode == STATE.running) {
         mode = STATE.finish
+        music.playTone(200, 200)
         radio.sendString("stop")
 
     }
